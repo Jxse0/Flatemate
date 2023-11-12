2cda0b7fba6274ee341470e36f866967ac8f511b
studmaiema1685 <manuel.maier@stud.th-rosenheim.de>
2023-11-12 12:33:53 +0100

    fix typo

--- backend/src/auth/index.ts
+++ backend/src/auth/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authController from "./controller";
-import validate from "../middleware/valitator";
+import validate from "../middleware/validator";
 import * as auth_validator from "../validations/auth";
 
 const authRouter = Router();
--- backend/src/user/index.ts
+++ backend/src/user/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import controller from "./controller";
-import validate from "../middleware/valitator";
+import validate from "../middleware/validator";
 import * as user_validate from "../validations/user";
 import { authenticateToken } from "../auth/authenticateToken";
 
