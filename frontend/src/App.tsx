ae01b5dd70751616683c5a3e65f9e84966b7c5b8
studhossno6561 <noah.hossbach@stud.th-rosenheim.de>
2023-12-06 16:11:23 +0100

    darkmode moved, styling sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,40 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import SignUp from "./components/SignUp";
-import useLocalStorage from "use-local-storage";
+import React, { useEffect, useState } from "react";
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { IconButton } from "@mui/material";
-import SidebarLeft from "./components/sidebar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import useLocalStorage from "use-local-storage";
+import "./App.css";
+import SidebarLeft from "./components/sidebar";
+import SignUp from "./components/SignUp";
 
 function App() {
-  const [count, setCount] = useState(0);
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
   const switchTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";    
+    const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   }
-  
+
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
-
   return (
-    <div className="App">
-      <SidebarLeft/>
-      <SignUp />
+    <Router>
+      <div className="App">
+        <SidebarLeft />
+        <Routes>
+          <Route path="/signup" Component={SignUp} />
+        </Routes>
         Switch to {theme === "light" ? "dark" : "light" } Theme
-      <IconButton sx={{ ml: 1 }} onClick={switchTheme} color="inherit">
-        {theme === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </div>
+        <IconButton sx={{ ml: 1 }} onClick={switchTheme} color="inherit">
+          {theme === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,27 +1,38 @@
-import React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 import MailIcon from '@mui/icons-material/Mail';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import { Icon } from '@mui/material';
 import './SidebarLeft.css';
+import { IconButton } from '@mui/material';
+import useLocalStorage from 'use-local-storage';
+import { useEffect } from 'react';
 
 const drawerWidth = 240;
-
-
 const SidebarLeft = () => {
+  
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+
     const items = [
         { text: 'Inbox', icon: <InboxIcon /> },
         { text: 'Starred', icon: <MailIcon /> },
@@ -31,13 +42,14 @@ const SidebarLeft = () => {
     
   return (
     <Box sx={{ display: 'flex' }}>
-      <Drawer className='sidebar'
+      <Drawer 
         sx={{
           width: drawerWidth,
           flexShrink: 0,
           '& .MuiDrawer-paper': {
             width: drawerWidth,
             boxSizing: 'border-box',
+            backgroundColor: 'var(--sidebar-color)',
           },
         }}
         variant="permanent"
@@ -45,7 +57,13 @@ const SidebarLeft = () => {
       >
         
         <div style={{backgroundImage: 'url(/abstract2.jpg)',  backgroundPosition: 'left'}} className='sidebar-top'>
-            <AccountCircleIcon className='profile-icon' fontSize='large'/>
+            <ListItemButton>
+              <ListItemIcon>
+              <AccountCircleIcon fontSize='large' />
+              </ListItemIcon>
+            <ListItemText primary="Account"/>
+            </ListItemButton>
+            
         </div>
         <Divider />
         <List>
@@ -71,6 +89,17 @@ const SidebarLeft = () => {
             </ListItem>
           ))}
         </List>
+        <Divider/>
+        <ListItemButton onClick={switchTheme} sx={{ position: 'absolute', bottom: 0 ,width:'100%'}}>
+          <ListItemIcon>
+          <IconButton>
+          {theme === 'light' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+          </ListItemIcon>
+          <ListItemText>
+          {theme === "light" ? "Light" : "Dark" } Mode
+          </ListItemText>
+        </ListItemButton>
       </Drawer>
     </Box>
   );
