4e3b1d217d38ba5aeed29b16b6355227dc58c28a
Jxse0 <jm.expositojavier@gmail.com>
2024-01-19 17:44:54 +0100

    message Style

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import "./Chat.css";
 import ChatApi from "../../api/ChatApi";
 import { tokenContext, userContext } from "../../InfoProvider";
@@ -9,16 +9,35 @@ type Message = {
   username: string;
 };
 
+function formatMessageDate(dateString: string) {
+  const date = new Date(dateString);
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Monate beginnen bei 0
+  const year = date.getFullYear().toString().substr(-2); // Letzten zwei Ziffern des Jahres
+  return `${hours}:${minutes} ${day}-${month}-${year}`;
+}
+
 const Chat: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState<string>("");
   const [token] = useContext(tokenContext);
   const [user] = useContext(userContext);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   useEffect(() => {
     const initMessages = async () => {
       const fetchedMessages = await ChatApi.fetchMessages(token);
-      setMessages(fetchedMessages);
+      const formattedMessages = fetchedMessages.map((message: Message) => ({
+        ...message,
+        time: formatMessageDate(message.time),
+      }));
+      setMessages(Array.isArray(formattedMessages) ? formattedMessages : []);
     };
 
     initMessages();
@@ -36,34 +55,44 @@ const Chat: React.FC = () => {
         username: data.username,
       };
 
-      setMessages((prevMessages) => [...prevMessages, incomingMessage]);
+      setMessages((prevMessages) =>
+        Array.isArray(prevMessages)
+          ? [...prevMessages, incomingMessage]
+          : [incomingMessage]
+      );
     });
 
     return () => {
       socket.close();
     };
-  }, []);
+  }, [token]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await ChatApi.sendMessage(newMessage, user.username, token);
     setNewMessage("");
   };
-
   return (
     <div className="chat-container">
       <div className="messages-container">
-        {messages ? (
+        {messages.length > 0 ? (
           messages.map((message, index) => (
             <div key={index} className="message">
-              <div>{message.text}</div>
-              <div>{message.username}</div>
-              <div>{message.time}</div>
+              <div className="message-header">
+                <div className="message-username">{message.username}</div>
+                <div className="message-time">{message.time}</div>
+              </div>
+              <div className="message-text">{message.text}</div>
             </div>
           ))
         ) : (
           <div>No Messages</div>
         )}
+        <div ref={messagesEndRef} />
       </div>
       <form onSubmit={handleSubmit}>
         <textarea
